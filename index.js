6dd204a1acc962e82551d0976f67d532e1265beb
Milton <Milton@ARRMS-PF250235.valtech.com>
2023-03-28 15:55:15 -0300

    changed CORS Settings to test

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ const scraperController = require('./scraper-controller.js');
 app.use(cors());
 app.use(express.json());
 
+var corsOptions = {
+  "Access-Control-Allow-Origin":"https://three-capital.netlify.app/",
+  "Access-Control-Allow-Methods": "POST, PUT, PATCH, GET, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization"
+}
+
 //Start the browser and create a browser instance
 let browserInstance = browserObject.startBrowser();
 
-app.get('/results', async(req, res) => {
+app.get('/results',cors(corsOptions), async(req, res) => {
   try {
     // Pass the browser instance to the scraper controller
     const responseData = await scraperController(browserInstance)
@@ -27,5 +33,6 @@ app.post("/post", (req, res) => {
   });  
 
 const PORT = process.env.PORT || 8080;
+
   
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
