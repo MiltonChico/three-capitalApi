875787cf86397ec8f6c269b881091383bc3d1894
Milton <Milton@ARRMS-PF250235.valtech.com>
2023-03-29 15:33:40 -0300

    all to normally, nothing to do from here with CORS

--- index.js
+++ index.js
@@ -7,22 +7,16 @@ const scraperController = require('./scraper-controller.js');
 app.use(cors());
 app.use(express.json());
 
-var corsOptions = {
-  "Access-Control-Allow-Origin":"https://three-capital.netlify.app/",
-  "Access-Control-Allow-Methods": "POST, PUT, PATCH, GET, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization"
-}
-
 //Start the browser and create a browser instance
 let browserInstance = browserObject.startBrowser();
 
-app.get('/results',cors(corsOptions), async(req, res) => {
+app.get('/results', async(req, res) => {
 
-  res.set('Access-Control-Allow-Origin', '*');
   try {
     // Pass the browser instance to the scraper controller
     const responseData = await scraperController(browserInstance)
     res.json(responseData)
+    
   } catch (err){
     res.status(400).json(err)
     console.error(err)
